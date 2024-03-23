09d55636c487a5b4cb43e09728b644075d1cce3e
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-23 10:52:20 -0300

    start

---
+++ lib/grokking_algorithms.ex
@@ -0,0 +1,18 @@
+defmodule GrokkingAlgorithms do
+  @moduledoc """
+  Documentation for `GrokkingAlgorithms`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GrokkingAlgorithms.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/grokking_algorithms_test.exs
@@ -0,0 +1,8 @@
+defmodule GrokkingAlgorithmsTest do
+  use ExUnit.Case
+  doctest GrokkingAlgorithms
+
+  test "greets the world" do
+    assert GrokkingAlgorithms.hello() == :world
+  end
+end
